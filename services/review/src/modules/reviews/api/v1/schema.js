55f9efe26f21391b0c5adba5af1858fc89732554
Akalanka <akalankaperera128@gmail.com>
2023-04-27 03:13:45 +0530

    Minor fix in review svc schema

--- services/review/src/modules/reviews/api/v1/schema.js
+++ services/review/src/modules/reviews/api/v1/schema.js
@@ -2,6 +2,6 @@ import { Joi } from 'celebrate';
 
 export const addReviewSchema = Joi.object({
   entity_id: Joi.string().hex().length(24).required(),
-  entity_type: Joi.string().values('product', 'seller').required(),
+  entity_type: Joi.string().valid('product', 'seller').required(),
   content: Joi.string().required(),
 });

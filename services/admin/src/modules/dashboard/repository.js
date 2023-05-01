e6ff4d0f7abe886d4ddf5d6300b4c94c9d650b4b
QbicSquid <shifan611@gmail.com>
2023-05-01 21:46:10 +0530

    Fixed profit aggregation query

--- services/admin/src/modules/dashboard/repository.js
+++ services/admin/src/modules/dashboard/repository.js
@@ -24,12 +24,45 @@ export const getProfits = () => {
   const db = mongoose.connection.db;
   return db.collection('orders').aggregate([
     {
+      $unwind: "$products"
+    },
+    {
       $lookup: {
-         from: "products",
-         localField: "products._id",
-         foreignField: "_id",
-         as: "products.product"
+        from: "products",
+        localField: "products._id",
+        foreignField: "_id",
+        as: "products.data"
+      }
+    },
+    {
+      $unwind: "$products.data"
+    },
+    {
+      $group: {
+        _id: { $month: "$created_at" },
+        profit: { $sum: { $round: [{ $multiply: [{ $subtract: ["$products.data.selling_price", "$products.data.markup_price"] }, "$products.quantity"] }, 2] } }
+      }
+    },
+    {
+      $addFields: {
+        month_string: {
+          $let: {
+            vars: {
+              monthsInString: [, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+            },
+            in: {
+              $arrayElemAt: ['$$monthsInString', '$_id']
+            }
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        month: "$month_string",
+        profit: 1
       }
     }
-  ])
+  ]).toArray()
 };

03adce244dd5412c63d43c8e9f52ebfffa3eb4a9
QbicSquid <shifan611@gmail.com>
2023-05-01 17:49:32 +0530

    Added dashboard route

---
+++ frontend/src/pages/dashboard.jsx
@@ -0,0 +1,13 @@
+import Layout from '../components/layout';
+
+const Dashboard = () => {
+  return (
+    <Layout title="Dashboard">
+      <div class="w-full flex flex-wrap">
+        
+      </div>
+    </Layout>
+  );
+};
+
+export default Dashboard;

ed872fa81ffb98cd5e161db7b3e24d35cc4b5a64
QbicSquid <shifan611@gmail.com>
2023-05-01 17:49:32 +0530

    Added dashboard route

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -5,6 +5,7 @@ export { default as Login } from './auth/login';
 export { default as Register } from './auth/register';
 export { default as Verify } from './auth/verify';
 export { default as Contact } from './contact';
+export { default as Dashboard } from './dashboard';
 export { default as Profile } from './profile';
 export { default as Cart } from './cart';
 export * from './auth';

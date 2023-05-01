24d0596c8b113eaec991ad325a4145eb88e7e8c3
QbicSquid <shifan611@gmail.com>
2023-05-01 19:14:23 +0530

    Updated get totals query

--- services/admin/src/modules/dashboard/repository.js
+++ services/admin/src/modules/dashboard/repository.js
@@ -2,8 +2,20 @@ import mongoose from 'mongoose';
 
 export const getSystemTotals = async () => {
   const db = mongoose.connection.db;
-  const users = await db.collection('users').find().count();
+  const [users, orders, succeededPayments, failedPayments, reviews] = await Promise.all([
+    db.collection('users').estimatedDocumentCount(),
+    db.collection('orders').estimatedDocumentCount(),
+    db.collection('orders').countDocuments({ status: 'paid' }),
+    db.collection('orders').countDocuments({ status: 'confirmed', payment_id: { $exists: true } }),
+    db.collection('reviews').estimatedDocumentCount(),
+  ]);
   return {
     users,
+    orders,
+    payments: {
+      suceeded: succeededPayments,
+      failed: failedPayments,
+    },
+    reviews,
   };
 };
